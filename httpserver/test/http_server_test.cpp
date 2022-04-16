ab11efc18924fc7195411d53e7ce0b04ac4903e8
tomocat <tomocat@github.com>
2022-04-16 15:26:31 +0800

    done

--- httpserver/test/http_server_test.cpp
+++ httpserver/test/http_server_test.cpp
@@ -1,34 +1,18 @@
 #include "http_server.h"
+#include "logger.h"
+#include "jsoncpp/json.h"
 
-using httpserver::Request;
-using httpserver::Response;
-
-// void Hello(Request& req, Json::Value& root) {
-//     root["hello"] = "world";
-// }
-// void SayHello(Request& req, Json::Value& root) {
-//     root["name"] = req.get_param("name");
-//     root["age"] = atoi(req.get_param("age").c_str());
-// }
-
-void Sleep(Request& req, Response& root) {
+void echo(httpserver::HttpRequest& req, httpserver::HttpResponse& resp) {
+    std::string name = req.headers["name"];
+    Json::Value root;
+    root["name"] = name;
+    resp.body = root.toStyledString();
 }
 
-int main(int argc, char** args) {
-    if (argc < 2) {
-        log_error("usage: ./http_server_test [port]");
-        return -1;
-    }
+int main() {
+    httpserver::HttpServer http_server(8888);
+    http_server.RegisterHandler("/echo", echo);
 
-    httpserver::HttpServer http_server;
-    // http_server.AddMapping("/hello", Hello);
-    // http_server.AddMapping("/sayhello", SayHello);
-    http_server.AddMapping("/sleep", Sleep);
-
-    int port = atoi(args[1]);
-    int backlog = 100000;
-    int max_events = 1000;
-
-    http_server.Start(port, backlog, max_events);
+    http_server.Start();
     return 0;
 }
