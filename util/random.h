df390ec086c6cf8ece3634d7c125b24004379b81
root <root@LAPTOP-4LK3S0IN>
2023-04-15 22:38:04 +0800

    [Refactor]: delete thirdpkg

--- util/random.h
+++ util/random.h
@@ -8,23 +8,23 @@ static unsigned int seed = 123;
 
 /**
  * @brief return random value in the range [min, max]
- * 
- * @param min 
- * @param max 
- * @return int 
+ *
+ * @param min
+ * @param max
+ * @return int
  */
 inline int rand_int(int min, int max) {
-    return min + rand_r(&seed) % (max - min);
+  return min + rand_r(&seed) % (max - min);
 }
 
 /**
  * @brief return random value in the range [0, max]
- * 
- * @param max 
- * @return int 
+ *
+ * @param max
+ * @return int
  */
 inline int rand_int(int max) {
-    return rand_r(&seed) % max;
+  return rand_r(&seed) % max;
 }
 
 }  // namespace util
